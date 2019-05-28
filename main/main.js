5f093a27f68fc555a40cca2020d94fc806790adf
Z.Sun <zhaohuisun@foxmail.com>
2019-05-28 16:43:42 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    var re;
+    if(str==910)
+        re="._. ... ._.\n"+
+            "|_| ..| |.|\n"+
+            "..| ..| |_|\n";
+    if(str==256)
+        re="._. ._. ._.\n"+
+            "._| |_. |_.\n"+
+            "|_. ._| |_|\n";
+    if(str==7)
+        re="._.\n"+
+            "..|\n"+
+            "..|\n";
+    return re;
 };
