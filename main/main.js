f5b7f1f8d60bc970c44d8204e7eebdcfc8411618
ZhengYan49 <49430782+ZhengYan49@users.noreply.github.com>
2019-05-28 19:09:34 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a) {
+    var c;
+    if(a==910){
+       c= "._. ... ._.\n"+
+          "|_| ..| |.|\n"+
+          "..| ..| |_|\n");
+       return  c;
+    }       
+     if(a==256){
+       c= "._. ._. ._.\n"+
+           "._| |_. |_.\n"+
+           "|_. ._| |_|\n");
+       return  c;
+    }  
+    if(a==7){
+       c= "._.\n"+
+          "..|\n"+
+          "..|\n");
+       return  c;
+    } 
 };
