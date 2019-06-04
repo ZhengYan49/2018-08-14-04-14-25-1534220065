b15a4de795a6aea1eb0cb222f36f3d7d79acc59e
ZhengYan49 <49430782+ZhengYan49@users.noreply.github.com>
2019-06-04 15:54:53 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,19 +1,61 @@
-module.exports = function main(a) {
-    var c;
-    if(a==910){
-       c= "._. ... ._.\n"+
-          "|_| ..| |.|\n"+
-          "..| ..| |_|\n";
-    }       
-     if(a==256){
-       c= "._. ._. ._.\n"+
-           "._| |_. |_.\n"+
-           "|_. ._| |_|\n";
-    }  
-    if(a==7){
-       c= "._.\n"+
-          "..|\n"+
-          "..|\n";
-    } 
-    return c;
-};
+module.exports = function main(str) {
+    var t0 = ['._.','|.|','|_|'];
+    var t1 = ['...','..|','..|'];
+    var t2 = ['._.','._|','|_.'];
+    var t3 = ['._.','._|','._|'];
+    var t4 = ['...','|_|','..|'];
+    var t5 = ['._.','|_.','._|'];
+    var t6 = ['._.','|_.','|_|'];
+    var t7 = ['._.','..|','..|'];
+    var t8 = ['._.','|_|','|_|'];
+    var t9 = ['._.','|_|','..|'];
+    var i,j,t;
+    for (j = 0; j < 3; j++)
+    {
+        for (i = 0; i < str.length; i++) 
+        { 
+            switch(str[i])
+            {
+                case "0":
+                    t=t0[j];
+                    break;
+                case "1":
+                    t=t1[j];
+                    break;
+                case "2":
+                    t=t2[j];
+                    break;
+                case "3":
+                    t=t3[j];
+                    break;
+                case "4":
+                    t=t4[j];
+                    break;
+                case "5":
+                    t=t5[j];
+                    break;
+                case "6":
+                    t=t6[j];
+                    break;
+                case "7":
+                    t=t7[j];
+                    break;
+                case "8":
+                    t=t8[j];
+                    break;
+                case "9":
+                    t=t9[j];
+                    break;
+            }
+            if(i==0)
+            	te=t;
+	    else
+		te=te+" "+t;
+        }
+        if(j==0)
+		re=te+"\n";
+	else
+	re=re+te+"\n";
+    }
+    return re;
+}
